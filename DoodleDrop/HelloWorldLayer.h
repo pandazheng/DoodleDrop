d4ca5b48928ad30918608f001ea3bb6c8a876d1b
pandazheng <543828315@qq.com>
2013-07-24 08:07:25 -0700

    Initial Commit

---
+++ DoodleDrop/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  DoodleDrop
+//
+//  Created by panda zheng on 13-7-24.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ DoodleDrop/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DoodleDrop
+//
+//  Created by panda zheng on 13-7-24.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
